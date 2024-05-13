5f6509f73617ce0b50b2b85bf1452e2e8122b769
6w0s <167772882+6w0s@users.noreply.github.com>
2024-05-13 15:46:56 +1000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,15 @@
 game.StarterGui:SetCore("SendNotification", {
-        Title = "Update is ass",
-        Text = "buy aries premium(ball curve = dead)",
+        Title = "Advertisement",
+        Text = "buy aries premium",
         Icon = "",
-        Duration = 3 -- Duration in seconds
+        Duration = 3
     })
-setfpscap(30)
+
+game.StarterGui:SetCore("SendNotification", {
+        Title = "Advertisement",
+        Text = "only if you dont care about ball curve",
+        Icon = "",
+        Duration = 3
+    })
+
+setfpscap(40)
